b332392f7160b7dab6ee781c41392fd63f1ed67b
Erle Carrara <erle.carrara@buser.com.br>
2021-07-23 09:27:35 -0300

    find ./ -type f -exec sed -i 's;/Grupy-dojo-front/;/;g' {} \;
    Deve ter um jeito melhor de fazer isso rsrs

--- precache-manifest.c59ec33d4944c00123464f95eaab7cec.js
+++ precache-manifest.c59ec33d4944c00123464f95eaab7cec.js
@@ -1,22 +1,22 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "34401f6f6975d9b0f7d8d6729d32558f",
-    "url": "/Grupy-dojo-front/index.html"
+    "url": "/index.html"
   },
   {
     "revision": "ace0b27f34d04b808955",
-    "url": "/Grupy-dojo-front/static/css/main.8ad07c60.chunk.css"
+    "url": "/static/css/main.8ad07c60.chunk.css"
   },
   {
     "revision": "93333fc409465ef2d9e1",
-    "url": "/Grupy-dojo-front/static/js/2.23830df8.chunk.js"
+    "url": "/static/js/2.23830df8.chunk.js"
   },
   {
     "revision": "ace0b27f34d04b808955",
-    "url": "/Grupy-dojo-front/static/js/main.96559bd8.chunk.js"
+    "url": "/static/js/main.96559bd8.chunk.js"
   },
   {
     "revision": "c10c49f8d949f97e2030",
-    "url": "/Grupy-dojo-front/static/js/runtime~main.9b0b49a0.js"
+    "url": "/static/js/runtime~main.9b0b49a0.js"
   }
 ]);
