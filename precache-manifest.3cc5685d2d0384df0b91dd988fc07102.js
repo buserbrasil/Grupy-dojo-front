ec043b8b929f5c03a5b89c243a84fcca9e8c9ddf
João Monte <joao.monte@buser.com.br>
2021-07-23 00:54:28 -0300

    Updates

---
+++ precache-manifest.3cc5685d2d0384df0b91dd988fc07102.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2f297816f0359a6c07a706af0531c2b",
+    "url": "/Grupy-dojo-front/index.html"
+  },
+  {
+    "revision": "0fcfca61975dc076c8e3",
+    "url": "/Grupy-dojo-front/static/css/main.8ad07c60.chunk.css"
+  },
+  {
+    "revision": "5c33eb6afac527236e7c",
+    "url": "/Grupy-dojo-front/static/js/2.397acfd0.chunk.js"
+  },
+  {
+    "revision": "0fcfca61975dc076c8e3",
+    "url": "/Grupy-dojo-front/static/js/main.e1e20c9d.chunk.js"
+  },
+  {
+    "revision": "c10c49f8d949f97e2030",
+    "url": "/Grupy-dojo-front/static/js/runtime~main.9b0b49a0.js"
+  }
+]);
